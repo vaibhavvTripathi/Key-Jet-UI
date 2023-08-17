2dcc38e6d4ab2423e2b9ebd3c632553b2807b4cc
Yash Karakoti <Yash>
2023-08-17 12:05:11 +0530

    ui updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,90 +10,16 @@ import { ResultContext } from "./context/ResultContext";
 import { useRouter } from "next/navigation";
 import { ColorModeContext } from "@/theme";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
-import CompeteGraph from "./compete/page";
+import Type from "@/components/Type";
+import CompeteGraph from "@/components/CompeteGraph";
 
 const para: string = `become must head into order for should about lead find face stand never man when work day say against plan word time use general few through day up into hand you which there out which open under interest this still move little leave at it where`;
 export default function Home() {
-  const {
-    HandleKeyDown,
-    UserTypedParagraph,
-    DisplayTypedParagraph,
-    OriginalParagraph,
-  } = useContext(TypeContext);
-
-  const { toggleColorMode } = useContext(ColorModeContext);
-
-  const [hasStarted, setHasStarted] = useState<boolean>(false);
-  const [timeCount, setTimeCount] = useState<number>(0);
-  const router = useRouter();
-  const { calculate } = useContext(ResultContext);
-
-  useEffect(() => {
-    if (!hasStarted) return;
-    if (
-      timeCount === 30 ||
-      UserTypedParagraph.length > OriginalParagraph.length
-    ) {
-      router.push("/results");
-      return;
-    }
-    const interval = setInterval(() => {
-      console.log(timeCount);
-      calculate(timeCount, DisplayTypedParagraph);
-      setTimeCount((prev) => prev + 1);
-    }, 1000);
-    return () => clearInterval(interval);
-  }, [hasStarted, timeCount]);
-
-  const handleKeyStroke = (e: KeyboardEvent) => {
-    if (hasStarted === false) setHasStarted(true);
-    HandleKeyDown(e);
-  };
-  useEffect(() => {
-    window.addEventListener("keydown", handleKeyStroke);
-
-    return () => {
-      return window.removeEventListener("keydown", handleKeyStroke);
-    };
-  }, [UserTypedParagraph]);
+  
 
   return (
     <>
-      <CompeteGraph />
-      {/* <Typography variant="h4" sx={{ textAlign: "center", mb: 3, mt: 10 }}>
-        Press Any Key to Start
-      </Typography> */}
-
-      <Box
-        sx={{
-          display: "flex",
-          gap: 2,
-          p: 2,
-          flexWrap: "wrap",
-          width: "70%",
-          mx: "auto",
-        }}
-      >
-        <Typography variant="h5" sx={{ color: "grey", fontWeight: 600 }}>
-          {timeCount}
-        </Typography>
-
-        <Box
-          sx={{
-            display: "flex",
-            gap: 2,
-            flexWrap: "wrap",
-          }}
-        >
-          {DisplayTypedParagraph.map((word, index) => {
-            return (
-              <Box key={index}>
-                <WordVal letters={word} />
-              </Box>
-            );
-          })}
-        </Box>
-      </Box>
+      <Type />
     </>
   );
 }
---
+++ src/components/Type.tsx
@@ -0,0 +1,93 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+"use client";
+import { ResultContext } from "@/app/context/ResultContext";
+import { TypeContext } from "@/app/context/TypeContext";
+import { ColorModeContext } from "@/theme";
+import { Box, Typography } from "@mui/material";
+import { useRouter } from "next/navigation";
+import React, { useContext, useEffect, useState } from "react";
+import WordVal from "./WordVal";
+
+function Type() {
+  const {
+    HandleKeyDown,
+    UserTypedParagraph,
+    DisplayTypedParagraph,
+    OriginalParagraph,
+  } = useContext(TypeContext);
+
+  const { toggleColorMode } = useContext(ColorModeContext);
+
+  const [hasStarted, setHasStarted] = useState<boolean>(false);
+  const [timeCount, setTimeCount] = useState<number>(0);
+  const router = useRouter();
+  const { calculate } = useContext(ResultContext);
+
+  useEffect(() => {
+    if (!hasStarted) return;
+    if (
+      timeCount === 30 ||
+      UserTypedParagraph.length > OriginalParagraph.length
+    ) {
+      router.push("/results");
+      return;
+    }
+    const interval = setInterval(() => {
+      console.log(timeCount);
+      calculate(timeCount, DisplayTypedParagraph);
+      setTimeCount((prev) => prev + 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [hasStarted, timeCount]);
+
+  const handleKeyStroke = (e: KeyboardEvent) => {
+    if (hasStarted === false) setHasStarted(true);
+    HandleKeyDown(e);
+  };
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyStroke);
+
+    return () => {
+      return window.removeEventListener("keydown", handleKeyStroke);
+    };
+  }, [UserTypedParagraph]);
+  return (
+    <>
+      <Typography variant="h4" sx={{ textAlign: "center", mb: 3, mt: 10 }}>
+        Press Any Key to Start
+      </Typography>
+      <Box
+        sx={{
+          display: "flex",
+          gap: 2,
+          p: 2,
+          flexWrap: "wrap",
+          width: "70%",
+          mx: "auto",
+        }}
+      >
+        <Typography variant="h5" sx={{ color: "grey", fontWeight: 600 }}>
+          {timeCount}
+        </Typography>
+
+        <Box
+          sx={{
+            display: "flex",
+            gap: 2,
+            flexWrap: "wrap",
+          }}
+        >
+          {DisplayTypedParagraph.map((word, index) => {
+            return (
+              <Box key={index}>
+                <WordVal letters={word} />
+              </Box>
+            );
+          })}
+        </Box>
+      </Box>
+    </>
+  );
+}
+
+export default Type;
